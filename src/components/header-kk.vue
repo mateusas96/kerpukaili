6ec980be1e79da85836827562cffa35c5f622b61
Matas Mikušauskas <45999356+mateusas96@users.noreply.github.com>
2025-08-11 11:52:23 +0300

    header photo where are you?

--- src/components/header-kk.vue
+++ src/components/header-kk.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="flex shadow-md bg-[url('smile.webp')] bg-no-repeat bg-cover">
+  <div class="flex shadow-md bg-[url('/smile.webp')] bg-no-repeat bg-cover">
     <div class="flex flex-1 items-center bg-red-100/35 px-4">
       <div class="p-4 z-10 text-white flex w-full text-4xl sm:text-5xl items-center justify-center">
         {{ $t('IT_IS_NOT_FUR_IT_IS_GLITTER_TEXT') }}
