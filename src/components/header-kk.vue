b56b7108529246a27146970de44cd8701f1613d4
Matas Mikušauskas <45999356+mateusas96@users.noreply.github.com>
2025-08-11 21:34:57 +0300

    z-index

--- src/components/header-kk.vue
+++ src/components/header-kk.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <div class="flex shadow-md bg-[url('/smile.webp')] bg-no-repeat bg-cover">
+  <div class="flex z-10 shadow-md bg-[url('/smile.webp')] bg-no-repeat bg-cover">
     <div class="flex justify-center w-full">
       <div class="flex items-center px-4 w-full md:w-[700px] lg:w-[900px]">
-        <div class="p-4 z-10 text-white flex w-full text-4xl sm:text-5xl justify-center">
+        <div class="p-4 text-white flex w-full text-4xl sm:text-5xl justify-center">
           {{ $t('IT_IS_NOT_FUR_IT_IS_GLITTER_TEXT') }}
         </div>
         <language-select />
