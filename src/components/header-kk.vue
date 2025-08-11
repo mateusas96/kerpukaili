13b968e79fcd6037966a0822bb2ddf3507b64687
Matas Mikušauskas <45999356+mateusas96@users.noreply.github.com>
2025-08-11 12:40:07 +0300

    header width fix

--- src/components/header-kk.vue
+++ src/components/header-kk.vue
@@ -4,11 +4,13 @@
 
 <template>
   <div class="flex shadow-md bg-[url('/smile.webp')] bg-no-repeat bg-cover">
-    <div class="flex flex-1 items-center bg-red-100/35 px-4">
-      <div class="p-4 z-10 text-white flex w-full text-4xl sm:text-5xl items-center justify-center">
-        {{ $t('IT_IS_NOT_FUR_IT_IS_GLITTER_TEXT') }}
+    <div class="flex justify-center bg-red-100/35 w-full">
+      <div class="flex items-center px-4 w-full md:w-[700px] lg:w-[900px]">
+        <div class="p-4 z-10 text-white flex w-full text-4xl sm:text-5xl justify-center">
+          {{ $t('IT_IS_NOT_FUR_IT_IS_GLITTER_TEXT') }}
+        </div>
+        <language-select />
       </div>
-      <language-select />
     </div>
   </div>
 </template>
